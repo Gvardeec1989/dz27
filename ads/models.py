f72ad880cd0c2ae6db5292e15ab08a9b6030bcd0
lesson10_homework <FrendOfMan@yandex.ru>
2022-10-06 17:34:40 +0300

    коммит

--- ads/models.py
+++ ads/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 
+from users.models import User
 
-class Ad(models.Model):
-    name = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
-    price = models.PositiveIntegerField()
-    description = models.TextField(null=True)
-    address = models.CharField(max_length=100)
-    is_published = models.BooleanField(default=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=60)
+class Ad(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads')
+    price = models.PositiveIntegerField(default=0)
+    description = models.TextField(max_length=2000)
+    is_published = models.BooleanField(default=None)
+    image = models.ImageField(upload_to='images/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='ads')
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
 
     def __str__(self):
         return self.name
--- dz27/urls.py
+++ dz27/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from ads.views import *
+from dz27 import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,8 @@ urlpatterns = [
     path('cat/<int:pk>', CategoryDetailView.as_view()),
     path('ad/', AdView.as_view()),
     path('ad/<int:pk>', AdDetailView.as_view()),
+    path('user/', include("users.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
