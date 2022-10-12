c3f50bd745a00cab991a40d02378e3558534e60c
lesson10_homework <FrendOfMan@yandex.ru>
2022-10-12 14:56:59 +0300

    поправил вьюхи

--- ads/urls/ad_urls.py
+++ ads/urls/ad_urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<int:pk>/', AdDetailView.as_view()),
     path('create/', AdCreateView.as_view()),
     path('<int:pk>/upload_image/', AdUploadImageView.as_view()),
-    path('delete/<int:pk>/', AdDeleteView.as_view()),
-    path('update/<int:pk>/', AdUpdateView.as_view())
+    path('<int:pk>/delete/', AdDeleteView.as_view()),
+    path('<int:pk>/update/', AdUpdateView.as_view())
 ]
